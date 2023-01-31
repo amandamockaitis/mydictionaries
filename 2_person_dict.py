7f6173c7809d154f89348503fc01f9d612c78e0f
amandamockaitis <amanda_mockaitis1@baylor.edu>
2023-01-31 15:16:36 -0600

    modified files

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,34 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+# print(person)
+
+# print out the name of the spouse
+
+print(person["spouse"])
+
+# print out the name of the second child
+
+print(person["children"][1])
+
+# print out the name of the cat
+
+print(person["pets"]["cat"])
+
+# use a loop to print out the names of each child
+
+for x in person["children"]:
+    print(x)
+
+# use a loop to print out the pets in the following format:
+# the type of pet is: dog and the name of the pet is: Fido
+
+for key, values in person["pets"].items():
+    print(f"the type of pet is: {key} and the name of the pet is: {values}")
+
+
+pet_dict = person["pets"]
+
+for key, values in pet_dict.items():
+    print(f"the type of pet is: {key} and the name of the pet is: {values}")
