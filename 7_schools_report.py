4b5e2247a0e14c33834a854a0b6462c5e7845232
amandamockaitis <amanda_mockaitis1@baylor.edu>
2023-02-02 15:15:44 -0600

    modified file

--- 7_schools_report.py
+++ 7_schools_report.py
@@ -14,6 +14,25 @@ Copy that info here:
 Display report for all universities that have a graduation rate for Women over 50%
 Display report for all universities that have a total price for in-state students living off campus over $50,000
 
-
-
 """
+import json
+
+infile = open("school_data.json", "r")
+
+schools = json.load(infile)
+conf_schools = [372, 108, 107, 130]
+
+# how many schools are in the file
+
+print(len(schools))
+
+for school in schools:
+    if school["NCAA"]["NAIA conference number football (IC2020)"] in conf_schools:
+        if school["Graduation rate  women (DRVGR2020)"] > 75:
+            print(f"Name of University: {school['instnm']}")
+            print(
+                f"Graduation rate for women: {school['Graduation rate  women (DRVGR2020)']}"
+            )
+
+            print()
+            print()
